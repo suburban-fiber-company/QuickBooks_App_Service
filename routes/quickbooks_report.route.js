42b759604b4abdde861c0c6bb9e14244f9c832dd
Oyedele Olufemi <itz4mesays@gmail.com>
2023-09-28 08:13:28 +0100

    swagger doc addition

--- routes/quickbooks_report.route.js
+++ routes/quickbooks_report.route.js
@@ -111,6 +111,40 @@ router.get('/debt-ageing-list/:realmID', reportListController.getAccountReceivab
  */
 router.get('/aged-payable-list/:realmID', reportListController.getAccountPayables)
 
+/**
+ * @swagger
+ *  /v1/quickbooks/report/account-payable-aging-summary/{realmID}:
+*      get:
+ *          summary: Get Report on AgedPayable Summary (Account Payable Summary)
+ *          tags: [QuickBooks Report]
+ *          parameters:
+ *              -   in: header
+ *                  name: authorization
+ *                  type: string
+ *                  required: true
+ *                  description: Bearer token
+ *              -   in: path
+ *                  name: realmID
+ *                  type: string
+ *                  required: true
+ *              -   in: query
+ *                  name: start_date
+ *                  type: date
+ *                  example: 'y-m-d'
+ *                  required: true
+ *              -   in: query
+ *                  name: end_date
+ *                  type: date
+ *                  example: 'y-m-d'
+ *                  required: true
+ *          responses:
+ *              200:
+ *                  description: Success
+ *              401:
+ *                  description: Unauthorized
+ *              500:
+ *                  description: SystemFailureError from QuickBooks
+ */
 router.get('/account-payable-aging-summary/:realmID', reportListController.getAccountPayableAgingSummary)
 
 module.exports = router
